5400759ca1d0d393d4c613bd32aa63ec55b8f6b3
PattyWu <51217671+patrick7166@users.noreply.github.com>
2024-12-15 17:45:48 +0800

    sound button

---
+++ Assets/Scripts/Sound/OSCLow_level.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace OscJack
+{
+    public class OSCLow_level : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        OscClient client;
+        void Start()
+        {
+            client = new OscClient("127.0.0.1", 5566);
+            //client.Send("/jump",       // OSC address
+                //-90,     // First element
+                //2.0f,
+               // 1);
+        }
+        public void soundSend_Right90Spring()
+        {
+            client.Send("/spring",       // OSC address
+                90,     // First element
+                2.0f,
+                1);
+        }
+        public void soundSend_Left90Spring()
+        {
+            client.Send("/spring",       // OSC address
+                -90,     // First element
+                2.0f,
+                1);
+        }
+        public void soundSend_Right90Jump()
+        {
+            client.Send("/jump",       // OSC address
+                90,     // First element
+                2.0f,
+                1);
+        }
+        public void soundSend_Left90Jump()
+        {
+            client.Send("/jump",       // OSC address
+                -90,     // First element
+                2.0f,
+                1);
+        }
+
+
+    }
+
+}
